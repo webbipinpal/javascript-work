cb409da672b33585723e835519c6f8569079a633
web.bipinpal <web.bipinpal@gmail.com>
2018-05-01 19:18:09 +0530

    angularjs work

---
+++ Class_angularjs/radio_select_data_show_hide/custom.js
@@ -0,0 +1,8 @@
+var app = angular.module('myapp', []);
+app.controller('Ctrl', function($scope){
+	$scope.showList = 'menu1';
+	
+	$scope.isShown = function(showList){
+		return showList === $scope.showList;
+	}
+});
