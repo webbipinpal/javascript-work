43ef2da2e47711401337f5b1679debe5646a724c
web.bipinpal <web.bipinpal@gmail>
2018-05-02 07:36:51 +0530

    add comment line

--- Class_angularjs/radio_select_data_show_hide/custom.js
+++ Class_angularjs/radio_select_data_show_hide/custom.js
@@ -1,4 +1,30 @@
 
+$(document).ready(function(){
+	$('.accordion-content').slideUp();  // close all accordiaons
+	$('.accordion-title').click(function(){
+
+		var a =  $(this).attr('class');
+		//if (a.indexOf("active") >= 0)alert(a.indexOf("active"));
+		if(a === "accordion-title active"){
+		
+		$(this).removeClass("active");
+		$(this).next('.accordion-content').slideUp();
+		
+		}else{
+		
+		$('.accordion-title').removeClass('active');
+		$('.accordion-content').slideUp();
+		
+		$(this).addClass('active');
+		$(this).next('.accordion-content').slideDown();
+		
+		}
+
+	});
+	
+});
+
+/*====== radion input select and display content js start here ==== */
 var app = angular.module('myapp', []);
 app.controller('Ctrl', function($scope){
 	$scope.showList = 'menu1';
@@ -7,4 +33,7 @@ app.controller('Ctrl', function($scope){
 		return showList === $scope.showList;
 	}
 });
+
+/*====== radion input select and display content js end here ==== */
+
 
