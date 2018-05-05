d5b61257a09b7d99ab8b35e4b669e907d4004a64
MOURYA <raja.mourya@non.agilent.com>
2018-05-05 18:36:45 +0530

    repeat count

---
+++ repeat_count/example.js
@@ -0,0 +1,18 @@
+
+angular.module("appmodule", []).controller("cartController", cartController);
+var cartController = (function(){
+
+	var itemsCount = 0;
+	var  cartController = function($scope){
+
+
+     console.log("shdfsduifhudhfsdf");
+
+		$scope.colleges = [{"nid":"256","title":"College of Islamic Studies"},{"nid":"239","title":"College of Humanities & Social Sciences"},{"nid":"235","title":"College of Science & Engineering"},{"nid":"257","title":"College of Law & Public Policy"}];
+
+   }
+
+
+   })();
+
+
