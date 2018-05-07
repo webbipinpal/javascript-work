300e01407add42935e2b97623bd1ba9955bcd998
webbipinpal <web.bipinpal@gmail.com>
2018-05-07 10:44:57 +0530

    working on filter_count_item

--- Class_angularjs/filter_count_item/example.js
+++ Class_angularjs/filter_count_item/example.js
@@ -1,5 +1,188 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope){
+	
+	$scope.collage = [{
+		"name":"menu1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"item1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"list1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	}];
+	$scope.collage2 = [{
+		"name":"collage2 menu1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage2 item1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	}];
+	$scope.collage3 = [{
+		"name":"collage3 menu1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage3 item1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage3 item2",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage3 item3",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	}];
+	
+	$scope.collage4 = [{
+		"name":"collage4 menu1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage4 item1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage4 item2",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage4 item3",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage4 item4",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage4 item5",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	}];
+	$scope.collage5 = [{
+		"name":"collage5 menu1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	}];
+	$scope.collage6 = [{
+		"name":"collage6 menu1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage6 menu1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	}];
+	$scope.collage7 = [{
+		"name":"collage7 menu1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage7 menu1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage7 menu1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage7 menu1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	},
+	{
+		"name":"collage7 menu1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564"
+		
+	}];
+
+	
+    $scope.a = $scope.collage.length;
+	$scope.b = $scope.collage2.length;
+	$scope.c = $scope.collage3.length;
+	$scope.d = $scope.collage4.length;
+	$scope.e = $scope.collage5.length;
+	$scope.f = $scope.collage6.length;
+	$scope.g = $scope.collage7.length;
+	$scope.allcount = $scope.a + $scope.b + $scope.c + $scope.d + $scope.e + $scope.f + $scope.g;	
+	
+	$scope.isShown = true;
+	$scope.myClick = function(){
+		$scope.isShown = false;
+		
+	}
+	
+
+	
+	/*
 	$scope.MenuItem1 = false;
 	$scope.item1 = function(){
 		$scope.MenuItem1 = !$scope.MenuItem1;
@@ -12,4 +195,7 @@ app.controller('myCtrl', function($scope){
 	$scope.item3 = function(){
 		$scope.MenuItem3 = !$scope.MenuItem3;
 	}
+	*/
 });
+
+
