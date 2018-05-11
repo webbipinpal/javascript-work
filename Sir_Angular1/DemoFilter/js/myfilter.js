12ba32c40dfb4209075d00e9e4c06fbcb3b3f0e0
webbipinpal <web.bipinpal@gmail.com>
2018-05-11 10:52:16 +0530

    added filter exm file which sir have provided and working filter _count_item

---
+++ Sir_Angular1/DemoFilter/js/myfilter.js
@@ -0,0 +1,72 @@
+
+/*
+
+Syntax of the Custome Filter
+app.filter("customeFilterName",function(){
+    return function(inputDataList,OptionalParm1,Optionalparm2){
+    var output;
+                                                             //////Cutome filter code here Note: inputDataList is Compulsory paramenter
+    return output;
+    
+    }
+});
+
+Discription:
+
+where inputDataList (name) given below in Desing View
+{{ name | CustomeFilterNmae:parm1:parm2}}
+
+*/
+
+app.filter("myFilter",function(){
+    return function(input){
+        return input.substring(0,1).toUpperCase();
+    }
+});
+app.filter("myFilter1",function(){
+    return function(input,option){
+        return input.substring(0,option).toUpperCase();
+    }
+});
+app.filter("myFilter2",function(){
+    return function(input,option){
+        if(isNaN(option) || option== ""){
+            return input;
+        }
+        else
+        {
+            return input.substring(input,option).toUpperCase();
+        }    
+    }
+});
+app.filter("idFilter",function(){
+    return function(input){
+        var output=[];
+        angular.forEach(input,function(value,key){
+            if(value.id>2)
+                {
+                    output.push(value);
+                }
+        });
+        return output;
+    }
+});
+app.filter("idFilter1",function(){
+    return function(input,option){
+        var output=[];
+        if(isNaN(option)|| option=="")
+        {
+            return input;
+        }
+        else{
+            angular.forEach(input,function(value,key){
+            if(value.id>option)
+            {
+                output.push(value);
+            }
+            });
+            return output;
+        }
+        
+    }
+});
