dccc3a37e1006a401450ca71b6b662ae00cf9ca2
webbipinpal <web.bipinpal@gmail.com>
2018-05-24 09:02:29 +0530

    added backup file

---
+++ Sir_Angular1/filter_count_item_final -22-5-2018/js/example.js
@@ -0,0 +1,335 @@
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope){
+	
+	//college name left side nav name
+	$scope.collegeName = [
+	
+		{"colName":"college Name 1", "type":"college1"},
+		{"colName":"college Name 2", "type":"college2"},
+		{"colName":"college Name 3", "type":"college3"},
+		{"colName":"college Name 4", "type":"college4"},
+		{"colName":"college Name 5", "type":"college5"}
+	
+	];
+	//select option years list
+	$scope.collegeListYear = [
+	
+		{"yearList":"2016"},
+		{"yearList":"2017"},
+		{"yearList":"2018"},
+		{"yearList":"2019"}
+	
+	];
+
+	//checkbox left side nav name
+	$scope.collegechecklist = [
+	
+		{"colName":"college Checkbox 1", "type":"checkbox1",status:false},
+		{"colName":"college Checkbox 2", "type":"checkbox2",status:false},
+		{"colName":"college Checkbox 3", "type":"checkbox3",status:false}
+	
+	];
+	
+
+	$scope.college = [{ 
+		"name":"college Name 1 Item 1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college1",
+        "chktype":"checkbox1",
+		"year":"2016",
+		"postDate":"May 20, 2016"
+		
+	}
+	,
+	{
+		"name":"college Name 1 Item 2",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college1",
+        "chktype":"checkbox3",
+		"year":"2016",
+		"postDate":"Jan 15, 2016"
+	},
+	{
+		"name":"college Name 1 Item 3",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college1",
+        "chktype":"checkbox2",
+		"year":"2016"
+		
+	},	
+	{
+		"name":"college Name 2 Item 1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college2",
+        "chktype":"checkbox2",
+		"year":"2017",
+		"postDate":"April 02, 2017"
+	},
+	{
+		"name":"college Name 2 Item 2",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college2",
+        "chktype":"checkbox2",
+		"year":"2017",
+		"postDate":"April 18, 2017"
+	},
+	{
+		"name":"college Name 3 Item 1",
+		"title":"best school",
+		"type":"college3",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college3",
+        "chktype":"checkbox3",
+		"year":"2018"
+		
+	},
+	{
+		"name":"college Name 3 Item 2",
+		"title":"best school",
+		"type":"college3",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college3",
+        "chktype":"checkbox3",
+		"year":"2018",
+		"postDate":"June 12, 2018"
+	},
+	{
+		"name":"college Name 3 Item 3",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college3",
+        "chktype":"checkbox3",
+		"year":"2016"
+		
+	},
+	{
+		"name":"college Name 3 Item 4",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+        "chktype":"checkbox3",
+		"type":"college3",
+		"year":"2017"
+	},
+	{
+		"name":"college Name 4 Item 1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college4",
+        "chktype":"checkbox4",
+		"year":"2018"
+		
+	},
+	{
+		"name":"college Name 4 Item 2",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college4",
+        "chktype":"checkbox4",
+		"year":"2019",
+		"postDate":"Sept 25, 2019"
+	},
+	{
+		"name":"college Name 4 Item 3",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college4",
+        "chktype":"checkbox4",
+		"year":"2017"
+		
+	},
+	{
+		"name":"college Name 4 Item 4",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college4",
+        "chktype":"checkbox4",
+		"year":"2018"
+		
+	},
+	{
+		"name":"college Name 4 Item 5",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college4",
+        "chktype":"checkbox4",
+		"year":"2019"
+		
+	},
+	{
+		"name":"college Name 4 Item 6",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college4",
+        "chktype":"checkbox4",
+		"year":"2017"
+	},
+	{
+		"name":"college Name 5 Item 1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"college5",
+        "chktype":"checkbox5",
+		"year":"2018"
+	},
+	{
+		"name":"college Checkbox 1 Item 1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"checkbox1",
+        "chktype":"checkbox1",
+		"year":"2016"
+	},
+	{
+		"name":"college Checkbox 1 Item 2",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"checkbox1",
+        "chktype":"checkbox1",
+		"year":"2017"
+	},
+	{
+		"name":"college Checkbox 2 Item 1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"checkbox2",
+        "chktype":"checkbox2",
+		"year":"2019"
+	},
+	{
+		"name":"college Checkbox 2 Item 2",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"checkbox2",
+        "chktype":"checkbox2",
+		"year":"2018"
+	},
+	{
+		"name":"college Checkbox 2 Item 3",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+        "chktype":"checkbox2",
+		"type":"checkbox2",
+		"year":"2019"
+	},
+	{
+		"name":"college Checkbox 2 Item 4",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"checkbox2",
+        "chktype":"checkbox2",
+		"year":"2017"
+	},
+	{
+		"name":"college Checkbox 3 Item 1",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"checkbox3",
+        "chktype":"checkbox3",
+		"year":"2016"
+	},
+	{
+		"name":"college Checkbox 3 Item 2",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"checkbox3",
+        "chktype":"checkbox3",
+		"year":"2016"
+	},
+	{
+		"name":"college Checkbox 3 Item 3",
+		"title":"best school",
+		"adds":"Fkj54524",
+		"cont":"564564564",
+		"type":"checkbox3",
+        "chktype":"checkbox3",
+		"year":"2016"
+	}];
+	
+	$scope.allitem = $scope.college.length;
+    
+	   //display product with year wise
+	$scope.Selectyear=$scope.collegeListYear[0];
+    
+    $scope.DisplayProList="";
+    
+	//count college item
+	$scope.getCollCount=function(type){
+        var count=0;
+        $scope.college.filter(function(x){
+            if(x.type==type)
+            {
+               count++;     
+            }
+            
+        });
+        return count;
+    }
+// count college with checkbox count
+$scope.getCollCountCheck=function(type){
+        var count=0;
+        $scope.college.filter(function(x){
+            if(x.type==type)
+            {
+               count++;     
+            }
+            
+        });
+        return count;
+    }
+// display on click college list then display related item
+
+	$scope.displayProd=function(type){
+
+		if(type=="all")
+		{
+			 $scope.DisplayProList="";
+		}
+		else
+        {
+			$scope.DisplayProList=type;
+		}	
+    }
+	
+	//clear filter function
+	 $scope.clearFilter = function(){
+		 //alert($scope.collegechecklist.length);
+		 for(var i = 0; i < $scope.collegechecklist.length; i++){
+			 $scope.collegechecklist[i].status = false;
+			 
+		 }
+	 }
+
+});
+
+
+
+
---
+++ Sir_Angular1/filter_count_item_final -22-5-2018/js/filterJs.js
@@ -0,0 +1,106 @@
+
+//display item with filter
+app.filter("filterAll",function(){
+    return function(input, displayProList, selectedYear, selectCheckBox,scope)
+    {
+      var output=[],allitem;
+        
+ /////////// Pick the check box which one selected begain
+      var chkItemList=[];
+	  scope.showFilter = false;
+       for(i=0;i<selectCheckBox.length;i++)
+        {
+               if(selectCheckBox[i].status==true)
+               {
+                   chkItemList.push(selectCheckBox[i].type);
+				   scope.showFilter = true;
+               }
+        }
+        console.log(chkItemList); // display selecet check box value which need to serach in collage array
+ /////////// Pick the check box which one selected end
+      console.log(displayProList);
+      console.log(selectedYear.yearList);
+      console.log(selectCheckBox);
+    // Year Filter begain 
+        if(selectedYear){
+            angular.forEach(input,function(value,key){
+            if(value.year == selectedYear.yearList){
+                console.log(value.year);
+                output.push(value);
+            }
+            });
+     // Year filter end         
+           
+             //display produch list related to college start here
+                 var CollageOutput=[]
+                 if(displayProList){
+                    angular.forEach(output,function(value,key){
+                    if(value.type == displayProList){
+                        console.log(value.year);
+                        CollageOutput.push(value);
+                    }
+                    });
+                     //// If There is Apply Collage name filter and check box both  then it apply the Same Filter for check box begain
+                    if(chkItemList.length>0) 
+                    {
+                        var FinalOutputList=[];
+                        angular.forEach(CollageOutput,function(value,key){
+                            console.log("Collage Filter"+value.chktype);
+                            angular.forEach(chkItemList,function(chkvalue,key){ // check each value which filter true value
+                                console.log("Check box"+chkvalue);
+                                if(value.chktype == chkvalue){
+                                    FinalOutputList.push(value);
+                                }
+                                
+                            });
+                       
+                        }); 
+                        console.log(FinalOutputList);
+						 scope.allitem=output.length;  // Get total Length according to year after 
+                        return FinalOutputList;
+                    }
+                      //// If There is Apply Collage name filter and check box both  then it apply the Same Filter for check box End
+                     else{
+                        scope.allitem=output.length;  // Get total Length according to year after filter 
+                        return CollageOutput;  
+                     }
+                   
+                 } /// Display Product List If Close
+                
+                else{  
+                    console.log("Display Collage Product List Else");
+                    //// If There is not click on the Collage name  then it apply the Same Filter for check box begain
+                    if(chkItemList.length>0) 
+                    {
+                        console.log("Display Collage Product List Else when check box Selected::: ");
+                        var FinalOutputList=[];
+                        // It filter the data On output array bcz it apply after year filter  
+                        angular.forEach(output,function(value,key){
+                            console.log("Collage Filter"+value);
+                            angular.forEach(chkItemList,function(chkvalue,key){ // check each value which filter true value
+                                console.log("Check box"+chkvalue);
+                                if(value.chktype == chkvalue){
+                                    FinalOutputList.push(value);
+                                }
+                                
+                            });
+                       
+                        }); 
+                        console.log(FinalOutputList);
+                        return FinalOutputList;
+                     //// If There is not click on the Collage name  then it apply the Same Filter for check box  end
+                    }
+                    else{
+                        console.log("Display Collage Product List Else when check box not Selected ");
+                        scope.allitem=output.length;  // Get total Length according to year after filter 
+                        return output; 
+                    }
+                    
+                   
+                }  // Colloage dispaly Product list Close
+            
+                //display produch list related to college start here
+            
+        }
+    }
+});
