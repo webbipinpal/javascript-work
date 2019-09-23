e681985fd103b61f9d0a5b03c0605811b9e85258
Bipin Kumar Pal <bpal@hbku.edu.qa>
2019-09-23 11:13:50 +0300

    added project

---
+++ Elearning/elearning-ar/javascript/main.js
@@ -0,0 +1,184 @@
+
+;(function($) {
+    var topSearch = function () {
+      $(document).on('click', function(e) {   
+            var clickID = e.target.id;   
+            if ( ( clickID !== 's' ) ) {
+                $('.top-search').removeClass('show');                
+            } 
+        });
+
+        $('.show-search').on('click', function(event){
+            event.stopPropagation();
+        });
+
+        $('.search-form').on('click', function(event){
+            event.stopPropagation();
+        });        
+
+        $('.show-search').on('click', function (event) {
+            if(!$('.top-search').hasClass( "show" )) {
+                $('.top-search').addClass('show');  
+                event.preventDefault();                
+            }
+                
+            else
+                $('.top-search').removeClass('show');
+                event.preventDefault();
+
+            if( !$('.show-search' ).hasClass( "active" ) )
+                $( '.show-search' ).addClass( 'active' );
+            else
+                $( '.show-search' ).removeClass( 'active' );
+        });   
+   
+    } 
+
+    var responsiveMenu = function() {
+        var menuType = 'desktop';
+
+        $(window).on('load resize', function() {
+            var currMenuType = 'desktop';
+
+            if ( matchMedia( 'only screen and (max-width: 991px)' ).matches ) {
+                currMenuType = 'mobile';
+            }
+
+            if ( currMenuType !== menuType ) {
+                menuType = currMenuType;
+
+                if ( currMenuType === 'mobile' ) {
+                    var $mobileMenu = $('#mainnav').attr('id', 'mainnav-mobi').hide();
+                    var hasChildMenu = $('#mainnav-mobi').find('li:has(ul)');
+
+                    $('.mega-menu .mega-menu-sub').hide();
+                    $('.has-mega-menu .submenu.mega-menu').hide();
+
+                    $('#header').after($mobileMenu);
+                    hasChildMenu.children('ul').hide();
+                    hasChildMenu.children('a').after('<span class="btn-submenu"></span>');
+                    $('.btn-menu').removeClass('active');
+                } else {
+                    var $desktopMenu = $('#mainnav-mobi').attr('id', 'mainnav').removeAttr('style');
+
+                    $desktopMenu.find('.submenu').removeAttr('style');
+                    $('#header').find('.wrap-nav').append($desktopMenu);
+                    $('.wrap-menu').find('.wrap-nav').append($desktopMenu);
+                    $('.btn-submenu').remove();
+                }
+            }
+        });
+
+        $('.btn-menu').on('click', function() {         
+            $('#mainnav-mobi').slideToggle(300);
+            $(this).toggleClass('active');
+        });
+
+        if ( matchMedia( 'only screen and (max-width: 991px)' ).matches ) {
+            $('.btn-mega').on('click', function() {      
+                $(this).parent('.sub-title').siblings().slideToggle(300);   
+                $(this).toggleClass('active');
+            });
+
+            $('.has-mega').on('click', function() {      
+                $(this).siblings().slideToggle(300);  
+                $(this).toggleClass('active');
+            });
+        } 
+
+        $(document).on('click', '#mainnav-mobi li .btn-submenu', function(e) {
+            $(this).toggleClass('active').next('ul').slideToggle(300);
+            e.stopImmediatePropagation()
+        });
+    }
+
+    var megaMenu = function() {
+        $('#mainnav li').each( function() {            
+            if ( $(this).hasClass('has-mega-menu') ) {
+                var $main_menu_possiton =  $(this).attr('id');                
+                var $megaMenu = $('.submenu-style2' +' .'+$main_menu_possiton );
+                $(this).append( $megaMenu );
+
+            } 
+        } );
+    }
+
+    var Accordion = function() {
+        var args = {duration: 600};
+
+        $('.flat-accordion .toggle-title').on('click', function () {
+            if( !$(this).is('.active') ) {
+                $(this).closest('.flat-accordion').find('.toggle-title.active').toggleClass('active').next().slideToggle(args);
+                $(this).toggleClass('active');
+                $(this).next().slideToggle(args);
+            } else {
+                $(this).toggleClass('active');
+                $(this).next().slideToggle(args);
+            }     
+        });
+    }; 
+ 
+      
+    var portfolioIsotope = function() {         
+        if ( $().isotope ) {           
+            var $container = $('.wrap-price-post .portfolio-wrap');
+            $container.imagesLoaded(function(){
+                $container.isotope({
+                    itemSelector: '.item',
+                    transitionDuration: '1s'
+                });
+            });
+
+            $('.wrap-price-post .portfolio-filter li ').on('click',function() {                           
+                var selector = $(this).find("a").attr('data-filter');
+                $('.wrap-price-post .portfolio-filter li').removeClass('active');
+                $(this).addClass('active');
+                $container.isotope({ filter: selector });
+                return false;
+            });
+
+            $('.flat-portfolio .load-more a').on('click', function(e) {
+                e.preventDefault();
+                return false;
+                var el = $(this),
+                    url = el.attr('href'),
+                    page = parseInt(el.attr('data-page'), 10);
+
+                el.addClass('loading').text('Loading...');
+
+                $.ajax({
+                    type: "GET",
+                    url: url,
+                    dataType: "html",
+                    async: false,   // wait result
+                    data : { page : page }
+                })
+                .done(function (data) {
+                    if ( data != null ) {                      
+                        var newitem = $(data);
+                        $container.append(newitem).isotope('appended', newitem);
+                        el.removeClass('loading').text('Load more');
+                        page = page + 1;
+                        el.attr({'data-page': page, 'href': './ajax/p' + page + '.html'});
+                    }
+                })
+                .fail(function () {
+                    el.text('No more portfolio to load.');
+                })
+            });
+        };
+    };
+
+
+/* Function
+ ------------------------------------------------------------------ */
+     $(function() {
+        topSearch();
+        responsiveMenu();
+        megaMenu();
+        Accordion();
+        portfolioIsotope();
+     });
+
+})(jQuery);
+
---
+++ Elearning/elearning-ar/javascript/slider.js
@@ -0,0 +1,38 @@
+$('.mainBanner .mainBanner__slider').slick({
+		dots:false,
+		dotsClass:'slider__dots',
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		speed: 1000,
+		arrows:true,
+		slidesToShow: 1,
+		rtl: true,
+		slidesToScroll: 1,
+		responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              dots:true,
+              arrows:false
+            }
+          }
+          ]
+	});
+
+/*tab js start here */
+
+$('ul.pro__cat__tab__meu li').on('click', function(){
+	var tab_id = $(this).attr('data-tab');
+	 localStorage.setItem('tab', tab_id);
+	$('ul.pro__cat__tab__meu li').removeClass('current');
+	$('.pro__tab--content').removeClass('current');
+	
+	$(this).addClass('current');
+	$('#'+tab_id).addClass('current');		
+	
+});
+/*tab js end here */
+$(document).ready(function(){
+	$(".noti-modal #myModal").modal('show');
+});
